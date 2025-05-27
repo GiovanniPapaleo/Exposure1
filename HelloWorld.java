e51c07a0783171a6aed6057a6314275c93f199c0
Giovanni <giovanni.papaleo@vtsd.com>
2025-05-27 13:56:03 -0400

    java

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+//Create class HelloWorld
+public class HelloWorld {
+
+    //Create main method
+    public static void main(String[] args) {
+        //Output to the window hello world
+        System.out.println("Hello World");
+
+
+
+    }
+
+}
---
+++ Introduction/VariableIntro.java
@@ -0,0 +1,26 @@
+package Introduction;
+
+public class VariableIntro {
+
+    public static void main(String[] args) {
+
+        /*
+        * To create a variable:
+        * datatype name = value;
+         */
+        double salary = 10000.56;
+        int bonus = 250;
+
+        /*
+        * Casting a variable means changing its datatype
+        * to change a primitive data type, put the new type in ()
+        * Example below.
+         */
+        bonus = (int) salary;
+
+        System.out.println(salary);
+        System.out.println(bonus);
+    }
+
+
+}
